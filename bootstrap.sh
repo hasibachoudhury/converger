b2087e1fbaaa4cfe8b0671f85c75451ce25136c9
Sumeet Singhal <sumeet.singhal@airbnb.com>
2017-05-01 00:23:51 -0700

    Adding initial scripts

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,7 @@ sudo su
 apt-get update
 apt-get install -y python3
 apt-get install git
+rm -rf converger
 git clone $1
+cd converger
+python3 converge_one_box.py
--- converge.py
+++ converge.py
@@ -2,5 +2,5 @@ import subprocess
 import json
 puppet = json.loads(open("puppet.json").read())
 for server in puppet["servers"]:
-  subprocess.run(["ssh", "vagrant@%s" %server, 'sudo apt-get update;sudo apt-get install -y python3'])
+  subprocess.run(["ssh", "vagrant@%s" %server, "%s %s" %(open(bootstrap.sh).read().replace("$1", puppet["git"]))])
   #ssh = subprocess.Popen(["ssh", "%s" %server, "python converge_one_box.py"])
